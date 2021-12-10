47d9d3d2b60000431a152820b3efd4eb8843eeee
Florian Sabani <florian.sabani@generali.com>
2021-12-11 00:44:13 +0100

    Fixing graphical line and circles.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
 import {register, login } from '../api'
 import React, { Fragment } from "react";
-import { Map, Marker, Overlay } from "pigeon-maps";
+import { Map, Marker, Overlay, OverlayProps } from "pigeon-maps";
 import { stamenToner } from 'pigeon-maps/providers'
 
 const appStyle = {
@@ -116,6 +116,48 @@ const HasJwt = () => {
     //{jwt && <div> Ha jwt </div>}
 }
 
+const Line = ({ mapState: { width, height },
+                  latLngToPixel,
+                  coordsArray,
+                  style = { stroke: 'rgb(255,0,0)', strokeWidth: 5 } }) => {
+    if (coordsArray.length < 2) {
+        return null;
+    }
+
+    let lines = []
+    let pixel = latLngToPixel(coordsArray[0])
+
+    lines.push(<circle cx={pixel[0]} cy={pixel[1]}
+                       r={style.strokeWidth}
+                       fill={style.stroke} />)
+
+    for (let i = 1; i < coordsArray.length; i++) {
+        let pixel2 = latLngToPixel(coordsArray[i])
+        lines.push(<line
+            key={i}
+            x1={pixel[0]}
+            y1={pixel[1]}
+            x2={pixel2[0]}
+            y2={pixel2[1]}
+            style={style}
+
+        />)
+        lines.push(<circle cx={pixel2[0]} cy={pixel2[1]}
+                           r={style.strokeWidth}
+                           fill={style.stroke} />)
+        pixel = pixel2
+    }
+
+    return (
+        <svg width={width} height={height}
+             style={{ top: 0, left: 0}}
+        >
+            {lines}
+        </svg>
+
+    )
+}
+
 
 class App extends React.Component {
     constructor(props){
@@ -144,29 +186,17 @@ class App extends React.Component {
         });
     };
 
+    markers = [[50.879, 4.6997],[50.8794, 4.6994]]
+
     map = (
         <Map
             provider={stamenToner}
-            defaultCenter={[50.879, 4.6997]}
+            defaultCenter={this.markers[0]}
             defaultZoom={18}
-            minZoom={18}
-            maxZoom={18}
             width={1000}
             height={1000}
         >
-            <Marker
-                anchor={[50.874, 4.6947]}
-                payload={1}
-                onClick={({ event, anchor, payload }) => {
-                    alert(JSON.stringify(payload));
-                }}
-            />
-            <Overlay
-                anchor={[50.879, 4.6997]} offset={[0, 0]}>
-                <svg height="20" width="20" style={overlayStyle}>
-                    <circle cx="0" cy="0" r="10" fill="red" />
-                </svg>
-            </Overlay>
+            <Line coordsArray={this.markers} />
         </Map>
     );
 
@@ -182,8 +212,10 @@ class App extends React.Component {
                 }
                 {
                     this.state.jwt &&
-                    <HasJwt/> &&
-                    <Fragment>{this.map}</Fragment>
+                    <div>
+                        <HasJwt/>
+                        <Fragment>{this.map}</Fragment>
+                    </div>
                 }
             </div>
         );

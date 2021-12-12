148e352ff6f426ae555f6973d31187d1df4edbc6
Florian Sabani <florian.sabani@generali.com>
2021-12-12 01:02:58 +0100

    Refactoring splitting components to files

--- src/containers/App/app.js
+++ src/containers/App/app.js
@@ -4,195 +4,20 @@ import {Map} from "pigeon-maps";
 import {stamenToner} from 'pigeon-maps/providers'
 import Popup from 'react-popup';
 import {IntervalExample} from "../components/interval";
+import {PathDrawer} from "../components/pathdrawer";
+import {HasJwt} from "../components/profile";
+import {LoginForm, RegisterForm} from "../components/auth";
 
 const appStyle = {
     height: '250px',
     display: 'flex'
 };
 
-const formStyle = {
-    margin: 'auto',
-    padding: '10px',
-    border: '1px solid #c9c9c9',
-    borderRadius: '5px',
-    background: '#f5f5f5',
-    width: '220px',
-    display: 'block'
-};
-
-const labelStyle = {
-    margin: '10px 0 5px 0',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-    fontSize: '15px',
-};
-
-const inputStyle = {
-    margin: '5px 0 10px 0',
-    padding: '5px',
-    border: '1px solid #bfbfbf',
-    borderRadius: '3px',
-    boxSizing: 'border-box',
-    width: '100%'
-};
-
-const submitStyle = {
-    margin: '10px 0 0 0',
-    padding: '7px 10px',
-    border: '1px solid #efffff',
-    borderRadius: '3px',
-    background: '#3085d6',
-    width: '100%',
-    fontSize: '15px',
-    color: 'white',
-    display: 'block'
-};
-
-const overlayStyle = {
-    overflow: 'visible'
-};
-
-const Field = React.forwardRef(({label, type, inputPlace}, ref) => {
-    return (
-        <div>
-            <label style={labelStyle}>{label}</label>
-            <input ref={ref} type={type} style={inputStyle} placeholder={inputPlace}/>
-        </div>
-    );
-});
-
-const LoginForm = ({onSubmit}) => {
-    const emailRef = React.useRef();
-    const passwordRef = React.useRef();
-    const handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-            email: emailRef.current.value,
-            password: passwordRef.current.value
-        };
-        onSubmit(data);
-    };
-    return (
-        <form style={formStyle} onSubmit={handleSubmit}>
-            <Field ref={emailRef} inputPlace="robimeyo@gmail.com" label="Email:" type="email"/>
-            <Field ref={passwordRef} label="Password:" type="password"/>
-            <div>
-                <button style={submitStyle} type="submit">Submit</button>
-            </div>
-        </form>
-    );
-};
-
-const RegisterForm = ({onSubmit}) => {
-    const nameRef = React.useRef();
-    const emailRef = React.useRef();
-    const passwordRef = React.useRef();
-    const handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-            name: nameRef.current.value,
-            email: emailRef.current.value,
-            password: passwordRef.current.value
-        };
-        onSubmit(data);
-    };
-    return (
-        <form style={formStyle} onSubmit={handleSubmit}>
-            <Field ref={nameRef} label="Name:" type="text"/>
-            <Field ref={emailRef} label="Email:" type="email"/>
-            <Field ref={passwordRef} label="Password:" type="password"/>
-            <div>
-                <button style={submitStyle} type="submit">Submit</button>
-            </div>
-        </form>
-    );
-};
-
-const HasJwt = ({user,onKidSelect}) => {
-    user = user?.data;
-    return (
-        <div>
-            <h1>Welcome {user.role} {user.name}!</h1>
-            <h3>
-                {user.email}[{user.verified ? 'ok' : 'nok'}]
-            </h3>
-            <ul>
-            {
-                user.childrens.map((kid,i)=> (
-                    <li>
-                        <div
-                            onClick={()=>onKidSelect(i)}
-                            style={{
-                            borderRadius:"50%",
-                            backgroundColor:kid.color,
-                            height: "25px",
-                            width: "25px"
-                        }}/>
-                        <span>{kid.name}</span>
-                    </li>
-                ))
-            }
-            </ul>
-            <h2>{new Date().getTime()}</h2>
-        </div>
-    );
-}
-
 function toArray(pos) {
     return [
         Number.parseFloat(pos.lat),
         Number.parseFloat(pos.long)
     ];
-}
-
-const PathDrawer = ({
-                  mapState: {width, height},
-                  latLngToPixel,
-                  kidsPaths,
-                  style = {strokeWidth: 5}
-              }) => {
-
-    function getPath(path) {
-        if (path.length < 2) {
-            return null;
-        }
-
-        let lines = []
-        let pixel = latLngToPixel(toArray(path[0]))
-
-        lines.push(<circle cx={pixel[0]} cy={pixel[1]}
-                           r={style.strokeWidth}
-                           fill={path[0].color}/>)
-
-        for (let i = 1; i < path.length; i++) {
-            let pixel2 = latLngToPixel(toArray(path[i]))
-            lines.push(<line
-                key={i}
-                x1={pixel[0]}
-                y1={pixel[1]}
-                x2={pixel2[0]}
-                y2={pixel2[1]}
-                style={
-                    {
-                        stroke: path[i].color,
-                        strokeWidth: 5
-                    }
-                }
-            />)
-            lines.push(<circle cx={pixel2[0]} cy={pixel2[1]}
-                               r={style.strokeWidth}
-                               fill={path[i].color}/>)
-            pixel = pixel2
-        }
-        return lines
-    }
-
-    return (
-        <svg width={width} height={height}
-             style={{top: 0, left: 0}}
-        >
-            {kidsPaths.map(kidPath=>getPath(kidPath))}
-        </svg>
-    )
 }
 
 
---
+++ src/containers/components/profile.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const HasJwt = ({user, onKidSelect}) => {
+    user = user?.data;
+    return (
+        <div>
+            <h1>Welcome {user.role} {user.name}!</h1>
+            <h3>
+                {user.email}[{user.verified ? 'ok' : 'nok'}]
+            </h3>
+            <ul>
+                {
+                    user.childrens.map((kid, i) => (
+                        <li>
+                            <div
+                                onClick={() => onKidSelect(i)}
+                                style={{
+                                    borderRadius: "50%",
+                                    backgroundColor: kid.color,
+                                    height: "25px",
+                                    width: "25px"
+                                }}/>
+                            <span>{kid.name}</span>
+                        </li>
+                    ))
+                }
+            </ul>
+            <h2>{new Date().getTime()}</h2>
+        </div>
+    );
+}
